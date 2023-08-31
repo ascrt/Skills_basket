9b2cb426165cc86f97dd1d8d58b79664e9808133
Michael <michael.mabonzo@gmail.com>
2023-08-31 11:42:11 +0200

    Style de la navigation et du Burger

--- default.php
+++ default.php
@@ -7,20 +7,31 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <nav>
-        <!-- Les liens -->
-        <ul>
-            <li> <a href="#"> Accueil </a></li>
-            <li> <a href="#"> Articles </a></li>
-            <li> <a href="#"> Login </a></li>
-            <li> <a href="#"> Inscription </a></li>
-        </ul>
-        <!-- Le burger -->
-        <button>
-            <span class="line l1"></span>
-            <span class="line l2"></span>
-            <span class="line l3"></span>
-        </button>
-    </nav>
+    <div class="grid-container">
+        <nav>
+            <!-- Les liens -->
+            <ul>
+                <li> <a href="#"> Accueil </a></li>
+                <li> <a href="#"> Articles </a></li>
+                <li> <a href="#"> Login </a></li>
+                <li> <a href="#"> Inscription </a></li>
+            </ul>
+            <!-- Le burger -->
+            <button>
+                <span class="line l1"> </span>
+                <span class="line l2"></span>
+                <span class="line l3"></span>
+            </button>
+        </nav>
+        <header>
+            <h1> Skills_Basket</h1>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+            <p> &copy; 2023 tout droit réservés</p>
+        </footer>
+    </div>
 </body>
 </html>
