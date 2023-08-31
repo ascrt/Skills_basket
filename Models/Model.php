daa90b1ad0f541b9d7d6de346efc864ec56c1f73
Michael <michael.mabonzo@gmail.com>
2023-08-31 15:55:36 +0200

    Classe Model terminé

--- Models/Model.php
+++ Models/Model.php
@@ -8,6 +8,40 @@ class Model extends Db {
     //Nom de la table de la base de données 
     protected string $table;
 
+    //Appel de db
+    private $db;
+
+    //Methode pour les requetes SQL
+    public function requete(string $sql, array $attributs = null) {
+        
+        //Instance de db
+        $this->db = Db::getInstance();
+        
+        //Verifie si il y a des attributs
+        if($attributs !== null) {
+
+            //Requete préparé
+            $query = $this->db->prepare($sql);
+            $query->execute($attributs);
+
+            return $query;
+
+        } else {
+
+            //Requete simple
+            return $this->db->query($sql);
+        }
+    }
+
+    //CRUD
+
+    /*** Read ***/
+    public function readAll() {
+        $query = $this->requete('SELECT * FROM'. $this->table);
+        return $query->fetchAll();
+    }
+
+
 }
 
 
