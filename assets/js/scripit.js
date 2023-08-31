181c514e219e8cfb33d5c7968e3fc816844b43b6
Michael <michael.mabonzo@gmail.com>
2023-08-31 12:06:46 +0200

    Header stylisé

--- assets/js/scripit.js
+++ assets/js/scripit.js
@@ -1,8 +1,7 @@
 const myNav = document.querySelector("nav ul");
-console.log(myNav);
 
 const myBurger = document.querySelector("#btn-burger");
-console.log(myBurger);
+
 
 myBurger.addEventListener('click', function(){
     myBurger.classList.toggle('active');
