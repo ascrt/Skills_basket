1176eb15b9bfd20c8711862c4d4beffd27237aed
Michael <michael.mabonzo@gmail.com>
2023-08-31 10:48:18 +0200

    Création du HTML

---
+++ default.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Skills_Basket</title>
+</head>
+<body>
+    <nav>
+        <!-- Les liens -->
+        <ul>
+            <li> <a href="#"> Accueil </a></li>
+            <li> <a href="#"> Articles </a></li>
+            <li> <a href="#"> Login </a></li>
+            <li> <a href="#"> Inscription </a></li>
+        </ul>
+        <!-- Le burger -->
+        <button>
+            <span class="line l1"></span>
+            <span class="line l2"></span>
+            <span class="line l3"></span>
+        </button>
+    </nav>
+</body>
+</html>
