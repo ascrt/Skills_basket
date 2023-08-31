681d19ca0b7c9ec62d4f99d5177e57a349dab888
Michael <michael.mabonzo@gmail.com>
2023-08-31 14:57:03 +0200

    Création d'un autoloader et de l'index

---
+++ Autoloader.php
@@ -0,0 +1,33 @@
+<?php 
+namespace App;
+
+class Autoloader {
+
+    static function register() {
+        spl_autoload_register([__CLASS__, 'autoload']);
+    }
+
+    static function autoload($class) {
+
+        /** On decoupe le namespace **/
+        $class = str_replace(__NAMESPACE__ . '\\', "", $class);
+        echo $class;
+
+        /** On remplace le \ par un / ***/
+        $class = str_replace("\\", "/", $class);
+        echo $class;
+
+        /*** On charge le fichier ***/
+        $fichier = __DIR__ . "/" . $class . ".php";
+
+        //On vérifie si le fichier existe
+        if(file_exists($fichier)) {
+            require_once $fichier;
+        } else {
+            echo "Ce fichier n'existe pas";
+        }
+    }
+}
+
+
+?>
