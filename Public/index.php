264525935a875024837e981880d1f6d84b7267c0
Michael <michael.mabonzo@gmail.com>
2023-09-01 10:19:15 +0200

    Creation de Main.php dans Core il sera notre router

--- Public/index.php
+++ Public/index.php
@@ -1,22 +1,5 @@
 <?php
 
-use App\Autoloader;
-use App\Db\Db;
-use App\Models\ArticlesModel;
-use App\Models\Model;
-
-require_once "Autoloader.php";
-Autoloader::register();
-
-$data = [
-    "title" => "Titre 3",
-    "content" => "Contenu",
-    "categoryId" => 2
-];
-
-$model = new ArticlesModel();
-$article = $model->hydrate($data);
-$model->update(2, $article);
-
+echo "VirtualHost pret";
 
 ?>
--- Views/default.php
+++ Views/default.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Skills_Basket</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../Public/assets/css/style.css">
 </head>
 <body>
     <div class="grid-container">
