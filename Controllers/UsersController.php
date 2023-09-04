006bb363864439380835bbcb155001c457ace035
Michael <michael.mabonzo@gmail.com>
2023-09-04 11:38:57 +0200

    Creation de UsersController dan controller

---
+++ Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Controllers;
+
+use App\Core\Form;
+
+class UsersController extends Controller {
+
+    public function login() {
+
+        //Formulaire de login
+        $form = new Form();
+    }
+
+}
+
+
+
+
+?>
